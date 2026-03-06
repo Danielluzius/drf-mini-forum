6a378c2a0a15a4a44761a8d394d7b78d0b45fbde
Daniel <Danielluzius@mail.de>
2026-03-06 13:27:44 +0100

    Implement POST request throttling in PostThrottle class

--- backend/forum_app/throttling.py
+++ backend/forum_app/throttling.py
@@ -3,3 +3,11 @@ from rest_framework.throttling import UserRateThrottle
 
 class PostThrottle(UserRateThrottle):
     scope = 'posts'
+    
+    def allow_request(self, request, view):
+        # Only POST requests are limited by this throttle
+        if request.method != 'POST':
+            return True
+        
+        # For POST requests: apply standard throttling
+        return super().allow_request(request, view)
